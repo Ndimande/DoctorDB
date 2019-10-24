41cad6b907f1738088eccc1e5ff2b4482df16723
Nqobani <37586727+Ndimande@users.noreply.github.com>
2019-10-24 10:07:56 +0200

    Create LoginModel.cs

---
+++ LoginModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorDBLibrary.Models
+{
+    public class LogicModel
+    {
+        public int userID { get; set; }
+        public string  username { get; set; }
+        public string  password { get; set; }
+        public string email { get; set; }
+        public string contactNumber { get; set; }
+    }
+}
